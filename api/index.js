f86d7d80048ec399b4445885a6f9c4c5ae88d7f2
Taralite <taralite@FVFXRCVBJ1WK.local>
2019-02-06 18:08:05 +0700

    add more routes to the api

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
+const UserHandler = require('./handlers/UserHandler');
+const BlogHandler = require('./handlers/BlogHandler');
+
 module.exports = {
     register: (server) => {
         server.route([
@@ -7,6 +10,31 @@ module.exports = {
                 handler: (request, h) => {
                     return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
                 }
+            },
+            {
+                method: 'POST',
+                path: '/login',
+                options: UserHandler.login
+            },
+            {
+                method: 'GET',
+                path: '/post/get/{pageNumber}',
+                options: BlogHandler.view
+            },
+            {
+                method: 'POST',
+                path: '/post/create',
+                options: BlogHandler.insert
+            },
+            {
+                method: "PUT",
+                path: '/post/update',
+                options: BlogHandler.update
+            },
+            {
+                method: 'DELETE',
+                path: '/post/delete',
+                options: BlogHandler.delete
             },
             {
                 method: 'GET',
